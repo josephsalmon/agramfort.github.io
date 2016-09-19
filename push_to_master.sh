f3eff99722c7ec63589ad147613935e12ff6f547
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-09-19 21:56:41 +0200

    iter

--- push_to_master.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env/bash
-# rsync -avzh ./output ../agramfort.github.io/
-cp  -R ./output/* ../agramfort.github.io/
-
-cd ../agramfort.github.io
-git ci -am 'updates'
-git commit -m 'rebuild pages' --allow-empty
-git push origin master
-cd ~/github/agramfort-site
